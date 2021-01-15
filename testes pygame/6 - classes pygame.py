9ee58559ca151b8c7497ae731b1f04a0ca9aadb2
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2021-01-15 20:40:38 -0300

    recriei a bola quicando nas bordas do jogo

--- testes pygame/6 - classes pygame.py
+++ testes pygame/6 - classes pygame.py
@@ -21,6 +21,15 @@ class Bola(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect[0] = LARGURA_JANELA/2
         self.rect[1] = ALTURA_JANELA/2
+        self.velocidadex = VELOCIDADEX
+        self.velocidadey = VELOCIDADEY
+    def mover(self):
+        if self.rect.left < 0 or self.rect.right > LARGURA_JANELA:
+            self.velocidadex = - self.velocidadex
+        if self.rect.top < 0 or self.rect.bottom > ALTURA_JANELA:
+            self.velocidadey = - self.velocidadey
+        self.rect[0] += self.velocidadex
+        self.rect[1] += self.velocidadey
 janela = pygame.display.set_mode((LARGURA_JANELA,ALTURA_JANELA))
 pygame.display.set_caption('Classes')
 
@@ -28,11 +37,16 @@ bola_grupo = pygame.sprite.Group()
 bola = Bola(BRANCO,TAMANHO_BOLA)
 bola_grupo.add(bola)
 
+relogio = pygame.time.Clock()
+
 deve_continuar = True
 while deve_continuar:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             deve_continuar = False
+    janela.fill(PRETO)
+    bola.mover()
     bola_grupo.update()
     bola_grupo.draw(janela)
     pygame.display.update()
+    relogio.tick(30)
